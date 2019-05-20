c67955e140c290941e4c05ae1f7b4a73e4d55a24
BanovJo <banov.josipa@gmail.com>
2019-05-20 17:22:19 +0200

    data

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace GUI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(126, 366);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GAME";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(132, 23);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "PLAY XO";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(338, 408);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe Script", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = " XO game";
+            this.TransparencyKey = System.Drawing.Color.Beige;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GUI/Form2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form2 : Form
+    {
+        bool turn = true;
+        int turn_count = 0;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+            checkForWiner();
+           
+        }
+
+        private void newGame1(object sender, EventArgs e)
+        {
+            new Form2().Show();
+            this.Hide();
+        }
+
+        private void checkForWiner()
+        {
+            bool winner = false;
+            if ((b11.Text == b12.Text) && (b12.Text == b13.Text) && (!b11.Enabled))
+                winner = true;
+            else if ((b21.Text == b22.Text) && (b22.Text == b23.Text) && (!b21.Enabled))
+                winner = true;
+            else if ((b31.Text == b32.Text) && (b32.Text == b33.Text) && (!b31.Enabled))
+                winner = true;
+            
+           else if ((b11.Text == b21.Text) && (b21.Text == b31.Text) && (!b11.Enabled))
+                winner = true;
+            else if ((b12.Text == b22.Text) && (b22.Text == b32.Text) && (!b12.Enabled))
+                winner = true;
+            else if ((b13.Text == b23.Text) && (b23.Text == b33.Text) && (!b13.Enabled))
+                winner = true;
+
+            else if ((b11.Text == b22.Text) && (b22.Text == b33.Text) && (!b11.Enabled))
+                winner = true;
+            else if ((b13.Text == b22.Text) && (b22.Text == b31.Text) && (!b13.Enabled))
+                winner = true;
+                
+            if (winner)
+            {
+              
+                String w_inner = "";
+                if (turn) w_inner = "O";
+                else w_inner = "X";
+
+                MessageBox.Show(w_inner + " WINS!");
+                nwGm();
+            }
+            else
+            {
+                if (turn_count == 9) {
+                    MessageBox.Show("Draw");
+                    nwGm();
+                }
+               
+            }
+            
+        }
+
+        private void nwGm()
+        {
+            new Form2().Show();
+            this.Hide();
+        }
+        }
+}
